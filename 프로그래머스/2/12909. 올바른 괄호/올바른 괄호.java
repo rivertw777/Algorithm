328c1cb87894120225576badd981e86d749f474f
Nicky <rivertw@naver.com>
2025-01-23 19:22:50 +0900

    [level 2] Title: 올바른 괄호, Time: 17.99 ms, Memory: 54.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;  
+        String res = "YES";
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                st.push(1);
+            } else if (s.charAt(i) == ')') {
+                if (st.isEmpty()) {
+                    answer = false;
+                    break;
+                } else {
+                    st.pop();
+                }
+            }
+        }
+
+        if(!st.isEmpty()) {
+            answer = false;
+        }
+
+        return answer;
+    }   
+}
