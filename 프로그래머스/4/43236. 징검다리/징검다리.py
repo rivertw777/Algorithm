e2ad94a68d46b56a4744a431d46c145a9540cbc9
Nicky <rivertw@naver.com>
2025-02-03 22:14:29 +0900

    [level 4] Title: 징검다리, Time: 179.93 ms, Memory: 12.4 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.py
+++ 프로그래머스/4/43236. 징검다리/징검다리.py
@@ -1,18 +1,18 @@
 def solution(distance, rocks, n):
     answer = 0
-    sorted_rocks = sorted(rocks)
-    sorted_rocks.append(distance)
+    rocks = sorted(rocks)
+    rocks.append(distance)
     left = 0
     right = distance
     while (left <= right):
         mid = int((left + right) / 2)
         cnt = 0
         p = 0
-        for i in range(len(sorted_rocks)):
-            if (sorted_rocks[i] - p < mid):
+        for i in range(len(rocks)):
+            if (rocks[i] - p < mid):
                 cnt += 1
             else:
-                p = sorted_rocks[i]
+                p = rocks[i]
         if cnt > n:
             right = mid - 1
         else:
