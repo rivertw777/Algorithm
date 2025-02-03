34b9938fff0d20145cf58f6ec0260239adcff759
Nicky <rivertw@naver.com>
2025-02-03 20:12:16 +0900

    [level 3] Title: 네트워크, Time: 0.98 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+def bfs(start, computers, visited):
+    q = deque([start])
+    visited[start] = True
+        
+    while q:
+        item = q.popleft()
+        for i in range(len(computers[item])):
+            if computers[item][i] != 0 and not visited[i]:
+                visited[i] = True
+                q.append(i)
+    
+def solution(n, computers):
+    visited = [False] * n
+    answer = 0
+    
+    for i in range(n):
+        if not visited[i]:  
+            bfs(i, computers, visited)
+            answer += 1
+        
+        print(visited)
+        
+    return answer
