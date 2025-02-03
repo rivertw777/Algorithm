2977cd2e3c98c93738235178f3e0f38729ca85a7
Nicky <rivertw@naver.com>
2025-02-03 21:35:03 +0900

    [level 3] Title: 여행경로, Time: 0.11 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -5,17 +5,28 @@ def solution(tickets):
             d[i[0]] = [i[1]]
         else:
             d[i[0]].append(i[1])
-            d[i[0]].sort()
-                    
+            
+    for i in d:
+        d[i].sort()
+    
+    print(d)
+    print()
+            
     st = ["ICN"]
     answer = []
     while st:
         start = st[-1]
         
         if start in d and d[start]:
+            
             item = d[start].pop(0)
             st.append(item)
+            
+            print(st, " - ", d)
+            if len(st) == len(tickets) + 1:
+                return st
+            
         else:
             answer.append(st.pop())
-            
+                        
     return answer[::-1]
