28d2621e8e8bf0739d38b0d2ef661e9440dfe89b
Nicky <rivertw@naver.com>
2025-02-04 15:49:38 +0900

    [level 3] Title: 순위, Time: 164.71 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.py
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -1,16 +1,25 @@
-from collections import defaultdict
-
 def solution(n, results):
-    answer = 0
-    win, lose = defaultdict(set), defaultdict(set)
+    total = [['?' for i in range(n)] for j in range(n)]
+
+    for i in range(n):
+        total[i][i] = 'SELF'
+
     for result in results:
-            lose[result[1]].add(result[0])
-            win[result[0]].add(result[1])
+        total[result[0]-1][result[1]-1] = 'WIN'
+        total[result[1]-1][result[0]-1] = 'LOSE'
 
-    for i in range(1, n + 1):
-        for winner in lose[i]: win[winner].update(win[i])
-        for loser in win[i]: lose[loser].update(lose[i])
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if total[i][k] == 'WIN' and total[k][j] == 'WIN':
+                    total[i][j] = 'WIN'
+                elif total[i][k] == 'LOSE' and total[k][j] == 'LOSE':
+                    total[i][j] = 'LOSE'
 
-    for i in range(1, n+1):
-        if len(win[i]) + len(lose[i]) == n - 1: answer += 1
+    answer = 0
+
+    for i in total:
+        if '?' not in i:
+            answer += 1
+
     return answer
