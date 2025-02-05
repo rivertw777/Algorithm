32c6337edac8b5a9703c2c270cee53409f34cded
Nicky <rivertw@naver.com>
2025-02-05 14:27:36 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.91 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,16 +1,20 @@
 def dfs(v, graph, visited):
     visited[v] = True
-    return sum([1] + [dfs(u, graph, visited) for u in graph[v] if not visited[u]])
+    count = 1  # 현재 노드 카운트
+    for u in graph[v]:
+        if not visited[u]:
+            count += dfs(u, graph, visited)
+    return count
 
 def solution(n, wires):
-    graph = [[] for _ in range(n+1)]
+    graph = [[] for _ in range(n + 1)]
     for v, u in wires:
         graph[v].append(u)
         graph[u].append(v)
 
     answer = 100
-    for i in range(n-1):
-        visited = [False for _ in range(n+1)]
+    for i in range(n - 1):
+        visited = [False for _ in range(n + 1)]
         v1, v2 = wires[i]
         visited[v2] = True
         tmp = abs(dfs(v1, graph, visited) - dfs(v2, graph, visited))
