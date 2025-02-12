658fde71f8cdd49a7c3a5bfb10acd8a6fc78875d
Nicky <rivertw@naver.com>
2025-02-12 21:18:04 +0900

    [level 3] Title: 숫자 게임, Time: 41.89 ms, Memory: 18.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.py
@@ -0,0 +1,12 @@
+def solution(A, B):
+    answer = 0
+    A.sort()
+    B.sort()
+    j = 0
+
+    for i in range(len(A)):
+        if A[j] < B[i]:
+            answer += 1
+            j += 1
+
+    return answer
