68b4549d42975991eacfe69363c769fcdb9dc768
Nicky <rivertw@naver.com>
2025-02-17 18:29:03 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 104.41 ms, Memory: 28.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -0,0 +1,35 @@
+def solution(gems):
+    answer = []
+    
+    total = len(set(gems))
+    
+    if total == 1:
+        return [1,1]
+    
+    temp = {}
+    temp[gems[0]] = 1
+    start, end = 1,1
+    while end <= len(gems):
+                        
+        if len(temp) < total:
+            if end == len(gems):
+                break
+            end += 1
+            
+            if gems[end-1] not in temp:
+                temp[gems[end-1]] = 1
+            else:
+                temp[gems[end-1]] += 1
+            
+        elif len(temp) == total:
+            answer.append((start, end))
+            
+            if temp[gems[start-1]] == 1:
+                del temp[gems[start-1]]
+            else:
+                temp[gems[start-1]] -= 1
+            
+            start += 1
+            
+    answer.sort(key = lambda x: x[1]-x[0])
+    return list(answer[0])
