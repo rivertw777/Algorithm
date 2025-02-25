7f9c6649fccef0e8513b25298d20849886e41089
Nicky <rivertw@naver.com>
2025-02-25 22:11:20 +0900

    [level 3] Title: 불량 사용자, Time: 303.51 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -1,27 +1,30 @@
 from itertools import permutations
 
-def is_matching(user_id, ban_id):
-    """주어진 user_id가 ban_id와 일치하는지 확인."""
-    if len(user_id) != len(ban_id):
+def is_matching(user_id, banned_id):
+    if len(user_id) != len(banned_id):
         return False
-    for u_char, b_char in zip(user_id, ban_id):
-        if b_char != '*' and u_char != b_char:
+    
+    for i in range(len(user_id)):
+        if banned_id[i] != '*' and banned_id[i] != user_id[i]:
             return False
+        
     return True
 
-def solution(user_id, banned_id):
+def solution(user_id, banned_id): 
+    ban_list = set()
+    for i in banned_id:
+        for j in user_id:
+            if is_matching(j, i):
+                ban_list.add(j)
+    
     answer = set()
-    banlist = set()
-
-    # 사용자 ID와 금지된 ID 매칭
-    for ban_id in banned_id:
-        for uid in user_id:
-            if is_matching(uid, ban_id):
-                banlist.add(uid)
-
-    # 가능한 모든 순열 생성
-    for per in permutations(banlist, len(banned_id)):
-        if all(is_matching(per[i], banned_id[i]) for i in range(len(per))):
-            answer.add(tuple(sorted(per)))  # 정렬하여 중복 방지
+    for per in permutations(ban_list, len(banned_id)):
+        flag = True  
+        for i in range(len(per)):
+            if not is_matching(per[i], banned_id[i]):
+                flag = False 
+                break  
+        if flag:
+            answer.add(tuple(sorted(per)))
 
     return len(answer)
