1c1f94e1354b71fe2cf4226da1ce2a782dbbfb2f
Nicky <rivertw@naver.com>
2025-02-28 20:38:02 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 253.49 ms, Memory: 31.7 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
@@ -1,36 +1,16 @@
-def max_sliding_window(arr):
-    max_sum = float('-inf')
-    current_sum = 0
-        
-    start = 0
-        
-    for end in range(len(arr)):
-        current_sum += arr[end]
-            
-        while start <= end and current_sum < 0:
-            current_sum -= arr[start]
-            start += 1
-            
-        max_sum = max(max_sum, current_sum)
-        
-    return max_sum
-
-def solution(seq):
-    p1 = []
-    p2 = []
-    
-    for i in range(len(seq)):
-        if i % 2 == 1:
-            p1.append(-1)
-            p2.append(1)
+def get_max(sequence, pulse):
+    sum_up = 0
+    ret = -1
+    for now in sequence:
+        if sum_up < 0:
+            sum_up = now * pulse
         else:
-            p1.append(1)
-            p2.append(-1)
-    
-    r1 = [a * b for a, b in zip(seq, p1)]
-    r2 = [a * b for a, b in zip(seq, p2)]
-        
-    max1 = max_sliding_window(r1)
-    max2 = max_sliding_window(r2)
-    
-    return max(max1, max2)
+            sum_up += now * pulse
+
+        ret = max(ret, sum_up)
+        pulse *= -1
+
+    return ret
+
+def solution(sequence):
+    return max(get_max(sequence, 1), get_max(sequence, -1))
