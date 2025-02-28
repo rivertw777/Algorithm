4af84f1866bb47994cd5536f825ba8571cf173a8
Nicky <rivertw@naver.com>
2025-02-28 19:42:13 +0900

    [level 3] Title: 입국심사, Time: 779.44 ms, Memory: 14.2 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,16 +1,20 @@
-def solution(n, times):
-    start, end =  1, times[-1] * n
-
+def solution(n, times):    
+    times.sort()
+    
+    start = 1
+    end = times[-1] * n
+    
     while start < end:
+        
         mid = (start + end) // 2
         
         total = 0
         for time in times:
             total += mid // time
-        
+                
         if total >= n:
-            end = mid
+            end = mid  
         else:
-            start = mid + 1
+            start =  mid + 1
     
     return end
