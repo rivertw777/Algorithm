081ce09ccaa5ee4b8644a32a7fd3b6f4af763c9d
Nicky <rivertw@naver.com>
2025-03-03 20:20:13 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 250.28 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
@@ -0,0 +1,20 @@
+def expand_around_center(s, left, right):
+    while left >= 0 and right < len(s) and s[left] == s[right]:
+        left -= 1
+        right += 1
+    return right - left - 1  # 팰린드롬의 길이 반환
+
+def solution(s):
+    if not s:
+        return 0
+    
+    max_length = 1
+    
+    for i in range(len(s)):
+        # 홀수 길이의 팰린드롬
+        len1 = expand_around_center(s, i, i)
+        # 짝수 길이의 팰린드롬
+        len2 = expand_around_center(s, i, i + 1)
+        max_length = max(max_length, len1, len2)
+    
+    return max_length
