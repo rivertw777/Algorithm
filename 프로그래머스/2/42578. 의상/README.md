bff939ac2e21bf6d2a136cc6947c7fc8813fdf6d
rivertw777 <rivertw@naver.com>
2025-04-11 20:44:21 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 69.9 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
 
 ### 성능 요약
 
-메모리: 90.4 MB, 시간: 0.11 ms
+메모리: 69.9 MB, 시간: 0.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 13일 22:29:53
+2025년 04월 11일 20:44:20
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,18 +2,16 @@ import java.util.HashMap;
 
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 1;
-        
         HashMap<String, Integer> hm = new HashMap<>();
+        
         for (int i = 0; i < clothes.length; i++) {
-            String key = clothes[i][1];
-            hm.put(key, hm.getOrDefault(key, 0) + 1);
+            hm.put(clothes[i][1], hm.getOrDefault(clothes[i][1], 0) + 1);
         }
         
-        for (int value : hm.values()) {
+        int answer = 1;
+        for (int value: hm.values()) {
             answer *= value + 1;
         }
-        
         return answer - 1;
     }
 }
