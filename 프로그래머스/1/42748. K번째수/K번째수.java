ded518a10a2696faba51a944108e84e9373488f1
rivertw777 <rivertw@naver.com>
2025-04-12 19:29:00 +0900

    [level 1] Title: K번째수, Time: 0.51 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,14 +1,14 @@
-import java.util.Arrays;
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-
-        for(int i=0; i<commands.length; i++){
-            int[] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
-            Arrays.sort(temp);
-            answer[i] = temp[commands[i][2]-1];
+        
+        for (int i = 0; i < commands.length; i++) {
+            int[] copy = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(copy);
+            answer[i] = copy[commands[i][2] - 1];
         }
-
         return answer;
     }
 }
