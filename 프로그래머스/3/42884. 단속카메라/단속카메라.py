b07f3e76add73fa13dbf7b5eab82c2088713caa4
rivertw777 <rivertw@naver.com>
2025-04-23 10:00:00 +0900

    [level 3] Title: 단속카메라, Time: 1.29 ms, Memory: 9.53 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,11 +1,13 @@
 def solution(routes):
     routes.sort(key=lambda x:x[1])
-    answer = 1
-    point = routes[0][1]
     
-    for r in routes:
-        if r[0] > point:
-            answer+=1
-            point = r[1]
+    answer = 1
+    index = 0
+    cur = routes[0][1]
+    while index < len(routes):
+        if cur < routes[index][0]:
+            cur = routes[index][1]
+            answer += 1
+        index += 1
         
     return answer
