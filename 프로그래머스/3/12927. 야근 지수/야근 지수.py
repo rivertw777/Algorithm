c06b50893368e1f0ea646c6b6e6a3ab35376fad1
rivertw777 <rivertw@naver.com>
2025-05-03 10:30:00 +0900

    [level 3] Title: 야근 지수, Time: 467.10 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.py
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.py
@@ -1,18 +1,21 @@
 import heapq as hq
 
 def solution(n, works):
-    if sum(works) <= n:
-        return 0
     
-    max_heap = [-work for work in works]
-    hq.heapify(max_heap)
+    q = []
+    for i in works:
+        hq.heappush(q, -i)
     
     for _ in range(n):
-        max_work = hq.heappop(max_heap)
-        hq.heappush(max_heap, max_work + 1)
-    
-    ans = 0
-    for i in max_heap:
-        ans += i ** 2
+        item = hq.heappop(q)
+
+        if item + 1 != 0:
+            hq.heappush(q, item + 1)
         
-    return ans
+        if not q:
+            break
+                        
+    answer = 0
+    for i in q:
+        answer += i * i
+    return answer
