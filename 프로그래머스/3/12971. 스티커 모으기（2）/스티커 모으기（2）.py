74feae4cb81e4628dd47d956295f8b2611a74308
rivertw777 <rivertw@naver.com>
2025-05-15 10:00:00 +0900

    [level 3] Title: 스티커 모으기(2), Time: 66.65 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
@@ -1,31 +1,27 @@
 def solution(sticker):
-    
-    if len(sticker) == 1:
+    answer = 0
+    n = len(sticker)
+
+    if n == 1:
         return sticker[0]
-    
-    if len(sticker) == 2:
+
+    if n == 2:
         return max(sticker[0], sticker[1])
-    
+
     # 첫 번째 포함, 마지막 미포함
     new = sticker[:-1]
-    dp = [0] * len(new)
-    dp[0] = new[0]
-    dp[1] = max(new[0], new[1])
-    
-    for i in range(2, len(new)):
-        dp[i] = max(dp[i-1], dp[i-2] + new[i])
-    
-    case1 = dp[-1]
-    
+    dp1 = [0] * (n - 1)
+    dp1[0] = new[0]
+    dp1[1] = max(new[0], new[1])
+    for i in range(2, n - 1):
+        dp1[i] = max(dp1[i-2] + new[i], dp1[i-1]) 
+
     # 첫 번째 미포함, 마지막 포함
     new = sticker[1:]
-    dp = [0] * len(new)
-    dp[0] = new[0]
-    dp[1] = max(new[0], new[1])
-    
-    for i in range(2, len(new)):
-        dp[i] = max(dp[i-1], dp[i-2] + new[i])
-    
-    case2 = dp[-1]    
-    
-    return max(case1, case2)
+    dp2 = [0] * n 
+    dp2[0] = new[0]
+    dp2[1] = max(new[0], new[1])
+    for i in range(2, n - 1):
+        dp2[i] = max(dp2[i-2] + new[i], dp2[i-1])
+
+    return max( max(dp1), max(dp2) )
