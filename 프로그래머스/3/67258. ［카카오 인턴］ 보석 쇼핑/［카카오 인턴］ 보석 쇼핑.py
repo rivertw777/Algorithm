7551342134661682e7662399c097de6c8b41d035
rivertw777 <rivertw@naver.com>
2025-05-16 10:00:00 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 102.13 ms, Memory: 27.1 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -1,34 +1,33 @@
-def solution(gems):
+def solution(gems):    
     total = len(set(gems))
     if total == 1:
         return [1,1]
+        
     
-    answer = []
-    
+    answer = []    
     temp = {}
-    temp[gems[0]] = 1
+    temp[gems[0]] = 1  #  딕셔너리에 보석 이름 저장
     start, end = 1,1
     while end <= len(gems):
-                        
-        if len(temp) < total:  
+        if len(temp) < total:  #  조건에 안맞으면 종료 포인트 증가
             if end == len(gems):
                 break
             end += 1
             
-            if gems[end-1] not in temp:
+            if gems[end-1] not in temp: # 현재 종료 지점의 보석이 없을때
                 temp[gems[end-1]] = 1
-            else:
+            else:                       # 있을 때
                 temp[gems[end-1]] += 1
             
-        elif len(temp) == total:
+        elif len(temp) == total: # 조건에 맞으면 시작 포인트 증가
             answer.append((start, end))
             
-            if temp[gems[start-1]] == 1:
+            if temp[gems[start-1]] == 1: # 현재 시작점의 보석이 1개일 때
                 del temp[gems[start-1]]
-            else:
-                temp[gems[start-1]] -= 1
+            else:                        # 그 이상일 때 
+                temp[gems[start-1]] -= 1 
             
-            start += 1
+            start += 1  
             
-    answer.sort(key = lambda x: x[1]-x[0])
+    answer.sort(key = lambda x: x[1] - x[0])
     return list(answer[0])
