e49d891384707ab7cc1924dc1a508518f5c7349b
rivertw777 <rivertw@naver.com>
2025-05-23 10:00:00 +0900

    [level 3] Title: 불량 사용자, Time: 127.00 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -1,5 +1,3 @@
-from itertools import permutations
-
 def is_matching(user_id, banned_id):
     if len(user_id) != len(banned_id):
         return False
@@ -10,21 +8,31 @@ def is_matching(user_id, banned_id):
         
     return True
 
-def solution(user_id, banned_id): 
-    ban_list = set()
-    for i in banned_id:
-        for j in user_id:
-            if is_matching(j, i):
-                ban_list.add(j)
-    
-    answer = set()
-    for per in permutations(ban_list, len(banned_id)):
-        flag = True  
-        for i in range(len(per)):
-            if not is_matching(per[i], banned_id[i]):
-                flag = False 
-                break  
-        if flag:
-            answer.add(tuple(sorted(per)))
+def dfs(same_lists, list_index, cur_list, answer_set):
 
-    return len(answer)
+    for el in same_lists[list_index]:
+        if el in cur_list:
+            continue
+        cur_list.append(el)
+        if len(cur_list) == len(same_lists):
+            answer_set.add(tuple(sorted(cur_list)))
+
+        if list_index + 1 < len(same_lists):
+            dfs(same_lists, list_index+1, cur_list, answer_set)
+        cur_list.remove(el)
+
+def solution(user_ids, banned_ids):
+    answer = 0
+    answer_set = set()
+    same_lists = []
+    for banned_id in banned_ids:
+        same_list = []  # banned_id당 일치하는 아이디들
+        for user_id in user_ids:
+            if is_matching(user_id, banned_id):
+                same_list.append(user_id)
+        same_lists.append(same_list)
+
+    new_tmp = []
+    dfs(same_lists, 0, new_tmp, answer_set)
+    
+    return len(answer_set)
