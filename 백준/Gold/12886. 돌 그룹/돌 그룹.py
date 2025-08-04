a5e9fbf61547267238c80dcbd63878e402c13923
rivertw777 <rivertw@naver.com>
2025-08-04 19:52:39 +0900

    [Gold IV] Title: 돌 그룹, Time: 284 ms, Memory: 50108 KB -BaekjoonHub

--- 백준/Gold/12886. 돌 그룹/돌 그룹.py
+++ 백준/Gold/12886. 돌 그룹/돌 그룹.py
@@ -1,32 +1,38 @@
-def BFS(s):
-    visited = [[False] * (total + 1) for _ in range(total+1)]
-    a,b = s[0],s[1]
-    Q = deque()
-    Q.append((a,b))
+from collections import deque
+
+def bfs(stone):
+    total = sum(stone)
+    visited = [[False] * (total + 1) for _ in range(total + 1)]
+    
+    a, b = stone[0], stone[1]
+    q = deque()
+    q.append((a,b))
     visited[a][b] = True
-    while Q:
-        x,y = Q.popleft()
+
+    while q:
+        x, y = q.popleft()
         z = total - x - y
+
         if x == y == z:
-            return True
-        for v1,v2 in [(x,y),(y,z),(z,x)]:
+            return 1
+
+        for v1, v2 in [(x,y), (y,z), (z,x)]:
             if v1 > v2:
-                v1,v2 = v2,v1
-            v2 = v2-v1
-            v1 = 2*v1
+                v1, v2 = v2, v1
+            v2 = v2 - v1
+            v1 = 2 * v1
             v3 = total - v1 - v2
-            v1,v2 = max(v1,v2,v3),min(v1,v2,v3)
+            v1, v2 = max(v1, v2, v3), min(v1, v2, v3)
+            
             if not visited[v1][v2]:
                 visited[v1][v2] = True
-                Q.append((v1,v2))
+                q.append((v1, v2))
 
-    return False
+    return 0
 
-from collections import deque
-s = sorted(list(map(int,input().split())))
-total = sum(s)
+stone = list(map(int,input().split()))
 
-if sum(s) % 3 != 0:
+if sum(stone) % 3 != 0:
     print(0)
 else:
-    print(int(BFS(s)))
+    print(bfs(sorted(stone)))
