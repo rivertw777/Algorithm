2c7275f4d797617abeceb0d67e61a8815f2412e7
rivertw777 <rivertw@naver.com>
2025-08-29 18:57:27 +0900

    [Gold II] Title: 친구 네트워크, Time: 324 ms, Memory: 62452 KB -BaekjoonHub

--- 백준/Gold/4195. 친구 네트워크/친구 네트워크.py
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.py
@@ -1,43 +1,32 @@
-T = int(input())
- 
-root = {}
-cnt = {}
- 
+from sys import stdin
+
+input = stdin.readline
+
 def find(x):
-    if root[x] == x:
-        return x
-    # 경로 압축
-    root[x] = find(root[x])
-    return root[x]
- 
-def union(x, y):
-    x = find(x)
-    y = find(y)
-    
-    if x == y:
-        return
-    
-    if x > y:
-        root[x] = y
-        # y를 root로 설정했으니, y의 네트워크 수에 x의 네트워크 수를 더해서 저장.
-        cnt[y] += cnt[x]
-    else:
-        root[y] = x
-        # x를 root로 설정했으니, x의 네트워크 수에 y의 네트워크 수를 더해서 저장.
-        cnt[x] += cnt[y]
- 
-for _ in range(T):
-    F = int(input())
-    root = {}   # Union-Find root
-    cnt = {}    # 친구 네트워크에 있는 사람 수
-    
-    for _ in range(F):
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
+
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+
+    if a != b:
+        parent[b] = a
+        number[a] += number[b]
+    print(number[a])
+
+
+for _ in range(int(input())):
+    num = int(input())
+    parent, number = {}, {}
+    for i in range(num):
         a, b = input().split()
-        if a not in root:
-            root[a] = a
-            cnt[a] = 1
-        if b not in root:
-            root[b] = b
-            cnt[b] = 1
+        if a not in parent:
+            parent[a] = a
+            number[a] = 1
+        if b not in parent:
+            parent[b] =b
+            number[b] = 1
         union(a, b)
-        print(cnt[find(a)])
