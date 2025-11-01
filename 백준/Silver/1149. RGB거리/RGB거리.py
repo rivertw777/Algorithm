4a4c018b3f9d96ba7385b4c6268862db9f1ad338
rivertw777 <rivertw@naver.com>
2025-11-01 17:32:20 +0900

    [Silver I] Title: RGB거리, Time: 56 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,18 +1,19 @@
 n = int(input())
-rgb = [list(map(int,input().split())) for _ in range(n)]
 
-ans = 10e9
+home = []
+for _ in range(n):
+    home.append( list(map(int, input().split())) )
 
-for i in range(3): # RGB 순서
-    dp = [[1001] * 3 for _ in range(n)]
+dp = [[0 for _ in range(n)] for _ in range(3)]
+
+dp[0][0] = home[0][0]
+dp[1][0] = home[0][1]
+dp[2][0] = home[0][2]
+
+for i in range(1, n):
     
-    dp[0][i] = rgb[0][i]
-    
-    for j in range(1, n):
-        dp[j][0] = min(dp[j-1][1], dp[j-1][2]) + rgb[j][0]
-        dp[j][1] = min(dp[j-1][0], dp[j-1][2]) + rgb[j][1]
-        dp[j][2] = min(dp[j-1][0], dp[j-1][1]) + rgb[j][2]
+    dp[0][i] = home[i][0] + min(dp[1][i-1], dp[2][i-1]) 
+    dp[1][i] = home[i][1] + min(dp[0][i-1], dp[2][i-1])
+    dp[2][i] = home[i][2] + min(dp[0][i-1], dp[1][i-1])
 
-    ans = min(ans, min(dp[-1]))
-
-print(ans)
+print( min(dp[0][-1], dp[1][-1], dp[2][-1]) )
