3b04fe2fb8f111292b23a83f419e9371b47bea06
rivertw777 <rivertw@naver.com>
2025-11-27 10:00:00 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 124 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.py
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.py
@@ -1,23 +1,19 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-dp = [0] * n
-dp[0] = 1
+dp = [1] * n
 for i in range(1, n):
     for j in range(i):
         if arr[j] < arr[i]:
-            dp[i] = max(dp[i], dp[j] + 1)
-       
-        if dp[i] == 0:
-            dp[i] = 1
+            dp[i] = max(dp[j] + 1, dp[i])
 
-answer = []
-big = max(dp)
-for i in range(n-1, -1, -1):
-    if dp[i] == big:
-        answer.append(arr[i])
-        big -= 1
+l = max(dp)
+ans = []
+for i in range(n-1, -1, -1):    
+    if dp[i] == l:
+        ans.append(arr[i])
+        l -= 1
 
 print(max(dp))
-for i in answer[::-1]:
+for i in ans[::-1]:
     print(i, end=' ')
