7eabf05b3e8b64fcf95a12f66f759aba19d917ab
rivertw777 <rivertw@naver.com>
2025-12-18 10:00:00 +0900

    [Silver III] Title: 01타일, Time: 344 ms, Memory: 71968 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+if n == 1 or n == 2:
+    print(n)
+else:
+    dp = [0]*(n+1)
+    dp[1] = 1
+    dp[2] = 2
+    
+    for i in range(3, n+1):
+        dp[i] = (dp[i-1] + dp[i-2]) % 15746
+    
+    print(dp[n])
