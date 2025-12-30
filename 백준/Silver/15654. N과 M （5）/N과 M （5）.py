87b5a3ad7a16b9da2399e9dc455aad77f8a751dd
rivertw777 <rivertw@naver.com>
2025-12-30 10:00:00 +0900

    [Silver III] Title: N과 M (5), Time: 200 ms, Memory: 39580 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -0,0 +1,22 @@
+def backtracking():
+    if len(array) == m:
+        result.add(tuple(array))
+        return
+
+    for i in num:
+        if i not in array:
+            array.append(i)
+            backtracking()
+            array.pop()
+
+n, m = map(int, input().split())
+num = list(map(int, input().split()))
+array = []
+result = set()
+
+backtracking()
+
+result = list(result)
+result.sort()
+for i in result:
+    print(" ".join(map(str, i)))
