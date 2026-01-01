6003b10f9f3ada8dc2c57bf7307d8c3d6fa183d7
rivertw777 <rivertw@naver.com>
2026-01-01 16:00:30 +0900

    [Silver III] Title: N과 M (3), Time: 1136 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,14 @@
+def backtracking():
+    if len(array) == m:
+        print(" ".join(map(str, array)))
+        return
+
+    for i in range(1, n+1):
+        array.append(i)
+        backtracking()
+        array.pop()
+
+n, m = map(int,input().split())
+array = []
+
+backtracking()
